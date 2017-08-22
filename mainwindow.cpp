a60df0d12b7f80ab4155790ad47994f5e0f58165
KapustaB <kapusta.borna.s@gmail.com>
2017-08-22 18:42:18 +0200

    Dodan msg box s obavijesti o vec dodanom linku

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "listchecker.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -14,13 +15,17 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void  makeNewMsgBoxLinkExists(){
+    QMessageBox::information(0, "Error", "Link already exists!");
+}
+
 void MainWindow::on_addLinkBtn_clicked()
 {
     QString link = ui->linkLineEdit->text();
     ListChecker listChecker;
 
     if (listChecker.checkIfListItemExist(link, ui->linkListWidget)){
-        //TODO
+        makeNewMsgBoxLinkExists();
         return;
      }
      ui->linkListWidget->addItem(link);
